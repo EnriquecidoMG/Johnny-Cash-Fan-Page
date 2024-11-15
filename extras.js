d8856a1bdb81121fb75afdd1e7b68ddae7fc095e
EnriquecidoMG <manuelmedinagomariz@gmail.com>
2024-11-15 09:51:34 +0100

    .JS Alert

---
+++ extras.js
@@ -0,0 +1,1 @@
+alert("This is not a real 'Fan Page' is a project for people who want to learn how to create a page like this in HTML, make sure you have readed the 'README.md' if you want to know the concept i used to explain here ");
